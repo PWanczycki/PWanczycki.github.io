7fb147da9eae3f6ed1b4764af2e79f8fdf482a08
Philip Wanczycki <p.wanczycki@gmail.com>
2024-03-09 23:22:13 -0500

    Remove default App and add bootstrap

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,13 @@
+function ListGroup() {
+  return (
+    <div>
+      <ul className="list-group">
+        <li className="list-group-item">Item 1</li>
+        <li className="list-group-item">Item 2</li>
+        <li className="list-group-item">Item 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ListGroup;
